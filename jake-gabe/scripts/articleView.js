cfc140c03c08e5dd2e31d696d45cabf4c72ca09e
gabrielx52 <gabriel.meringolo@gmail.com>
2017-09-18 09:07:07 -0700

    Completed second TODO, set up var for new articles, cleared out #articles element.

--- jake-gabe/scripts/articleView.js
+++ jake-gabe/scripts/articleView.js
@@ -129,8 +129,8 @@ articleView.initNewArticlePage = function() {
 articleView.create = function() {
   // TODO: Set up a var to hold the new article we are creating.
   // Clear out the #articles element, so we can put in the updated preview
-
-
+  var $newArticle = $('#article-template').html();
+  $('#articles').removeAttr('id');
   // TODO: Instantiate an article based on what's in the form fields:
 
 
@@ -151,4 +151,5 @@ articleView.initIndexPage = function() {
   articleView.handleAuthorFilter();
   articleView.handleMainNav();
   articleView.setTeasers();
+  articleView.create();
 };
